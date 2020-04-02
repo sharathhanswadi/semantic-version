0da83ff468ab3a76d1ffff96b48336c53c456ac0
sharath.hn <sarath.hn@triconinfotech.com>
2020-04-02 16:02:53 +0530

    feat: add the github for new version

--- src/main/java/com/vdurmont/semver4j/semantic.java
+++ src/main/java/com/vdurmont/semver4j/semantic.java
@@ -5,8 +5,8 @@ import com.vdurmont.semver4j.Semver;
 public class semantic {
 
 public  static void main(String[] args){ 
-Semver sem1 = new Semver("1.2.3-beta.4+sha899d8g79f87"); // Defaults to STRICT mode
-//Semver sem2 = new Semver("1.2.3-beta.4+sha899d8g79f87", SemverType.NPM); // Specify the mode
+//Semver sem1 = new Semver("1.2.3-beta.4+sha899d8g79f87"); // Defaults to STRICT mode
+Semver sem2 = new Semver("1.2.3-beta.4+sha899d8g79f87", Semver.SemverType.NPM); // Specify the mode
   }
 }
 
